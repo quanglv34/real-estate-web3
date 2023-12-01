2a9e9abff6f525ed301994e68ba97ddd21b0b324
Le Vu Quang <quanglv.341@gmail.com>
2023-12-01 22:13:13 +0700

    update contracts

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,41 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; 
+import "./RealEstateNFT.sol"; 
 
-contract RealEstate is ERC721, ERC721URIStorage {
-    uint256 private _nextTokenId;
+contract RealEstateMarketplace is RealEstateNFT {
+    address public erc20Address;
 
-    constructor(
-        address initialOwner
-    ) ERC721("RealEstate", "REAL") {}
-
-    function safeMint(
-        address to,
-        string memory uri
-    ) public returns (uint256) {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-        return tokenId;
+    constructor(address  _currency) RealEstateNFT(msg.sender) {
+        erc20Address = _currency;
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
+    Property[] properties;
+
+    // Struct
+    struct Property {
+        uint id;
+        IERC721 nft;
+        string name;
+        uint price;
+        address payable owner;
+        address payable seller;
+        bool sold;
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
-        return super.supportsInterface(interfaceId);
+    // EVENTS
+    event PropertyCreated (
+        uint tokenId,
+        string name,
+        string uri
+    );
+
+    event PropertyPurchased (
+        uint tokenId,
+        string price,
+        string uri
+    );
+
+    // Function 
+    function createProperty(string memory uri, uint256 _price) public {
+        require(msg.sender == owner(), "You are not owner");
+        require(_price > 0, "Product price should be greater than 0");
+
+        uint256 tokenId = safeMint(owner(), uri); 
+
+        Property memory newProduct;
+        newProduct.tokenId = tokenId; 
+        newProduct.price = _price;
+        newProduct.uri = uri;
+        tokenIdToProduct[tokenId] = newProduct;
+        
+        emit PropertyCreated(productCount, _name, _price, msg.sender, false);
+        products.push(newProduct); 
+
     }
 
-    function totalSupply() external view returns (uint256) {
-        return _nextTokenId;
+    function purchaseProperty(uint256 _tokenId) public { 
+        require(IERC20(erc20Address).allowance(msg.sender, address(this)) >= tokenIdToProduct[_tokenId].price, "insufficient approval");
+        IERC20(erc20Address).transferFrom(msg.sender, address(this), tokenIdToProduct[_tokenId].price); 
+        this.transferFrom(owner(), msg.sender, _tokenId);
+
+        emit PropertyPurchased(_id, myProduct.name, myProduct.price, seller, myProduct.owner, true);
     }
 
+    function getAllProperties() public view returns (Property[] memory) {
+        return products; 
+    }
 }
